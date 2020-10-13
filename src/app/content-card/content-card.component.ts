ef999263d3913f9e2f25b7e03a1634d52ccedcf5
Andrew McNeill <andrewjndmcneill@gmail.com>
2020-10-13 02:03:16 -0400

    Removed ContentList

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ContentList} from '../helper-files/content-list';
 import {Content} from '../helper-files/content-interface';
 
 @Component({
@@ -8,31 +7,8 @@ import {Content} from '../helper-files/content-interface';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-  contentList: ContentList;
 
   constructor() {
-    this.contentList = new ContentList();
-    this.contentList.add({
-      author: 'Name McNameyface',
-      body: 'Hello, my name is Name McNameyface. This is some text. How exciting.',
-      id: 0,
-      imgUrl: 'https://www.example.com/exampleimage.png',
-      tags: ['look', 'some', 'tags!'],
-      title: 'McNameyface\'s Content!'});
-    this.contentList.add({
-      author: 'Name McNameyface',
-      body: 'Hello, my name is Name McNameyface. This is some text. How exciting.',
-      id: 0,
-      imgUrl: 'https://www.example.com/exampleimage.png',
-      tags: ['look', 'some', 'tags!'],
-      title: 'Some more stuff'});
-    this.contentList.add({
-      author: 'Name McNameyface',
-      body: 'Hello, my name is Name McNameyface. This is some text. How exciting.',
-      id: 0,
-      imgUrl: 'https://www.example.com/exampleimage.png',
-      tags: ['look', 'some', 'tags!'],
-      title: 'Gotta pad content for the assignment!'});
   }
 
   ngOnInit(): void {
